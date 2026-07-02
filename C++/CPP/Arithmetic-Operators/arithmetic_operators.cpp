be45954f2653ddc88682f451fb97e4baee876a6e
Harshada2071 <harshada2071@gmail.com>
2026-07-02 22:41:40 +0530

    Implement basic arithmetic operations in C++

---
+++ C++/CPP/Arithmetic-Operators/arithmetic_operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Addition = " << a + b<<endl;
+    cout << "subtraction = " << a - b<<endl;
+    cout << "multiplication = " << a * b<<endl;
+    cout << "Division = " << a / b<<endl;
+    cout << "modulus = " << a % b<<endl;
+
+    return 0;
+}
