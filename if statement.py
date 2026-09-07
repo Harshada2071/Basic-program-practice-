f5735c48c74e239303c81c74ea7c2a457825b633
Harshada2071 <harshada2071@gmail.com>
2026-09-07 22:32:31 +0530

    Update and rename light.py to if statement.py

---
+++ if statement.py
@@ -0,0 +1,19 @@
+# if statement program 1st
+light = "red"
+
+  if light == "red":
+    print("stop")
+elif light == "green":
+    print ("go")
+elif light == "yellow":
+     print ("look")
+print ("end of the code")
+
+# if statement program 2nd
+age = 19
+if age < 18:
+    print ("minor")
+elif age == 18:
+    print ("just eligible for voting") 
+elif age > 18:    
+     print ("eligible for voting")
